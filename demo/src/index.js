cd7ec47c9447054b8a6fa5ba9082da802d772c31
Patrick Ramser <pramser@splashfinancial.com>
2020-05-22 15:23:16 -0400

    Make Demo a function component

--- demo/src/index.js
+++ demo/src/index.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { render } from 'react-dom';
 
 import TaskList from '../../src/task_list';
@@ -6,60 +6,48 @@ import TaskList from '../../src/task_list';
 import './index.css';
 import logo from './logo.svg';
 
-class Demo extends Component {
-  constructor(props) {
-    super(props);
+const Demo = () => {
 
-    this.state = {
-      tasks: []
-    };
-  }
+  const [tasks, setTasks] = useState(
+    [
+      {
+        id: 1,
+        title: 'Milk',
+        description: 'Do not forget milk at the grocery store.',
+        isCompleted: false
+      },
+      {
+        id: 2,
+        title: 'Weekly Enhancements',
+        description: 'Enhance this code to make it better and easier to use.',
+        isCompleted: false
+      },
+      {
+        id: 3,
+        title: 'React Tasks',
+        description: 'Make a component for easily building task lists.',
+        isCompleted: true
+      }
+    ]
+  );
 
-  componentDidMount() {
-    this.setState({
-      tasks: [
-        {
-          id: 1,
-          title: 'Milk',
-          description: 'Do not forget milk at the grocery store.',
-          isCompleted: false
-        },
-        {
-          id: 2,
-          title: 'Weekly Enhancements',
-          description: 'Enhance this code to make it better and easier to use.',
-          isCompleted: false
-        },
-        {
-          id: 3,
-          title: 'React Tasks',
-          description: 'Make a component for easily building task lists.',
-          isCompleted: true
-        }
-      ]
-    });
-  }
+  const onTaskChanged = task => {
+    var _tasks = tasks.slice();
+    var i = _tasks.findIndex(t => task.id === t.id);
+    _tasks[i] = task;
 
-  onTaskChanged = updatedTask => {
-    console.log(updatedTask);
-    var tasks = this.state.tasks.slice();
-    var updateIndex = tasks.findIndex(task => task.id === updatedTask.id);
-    tasks[updateIndex] = updatedTask;
-
-    this.setState({ tasks });
+    setTasks(_tasks);
   };
 
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>React Tasks</p>
-        </header>
-        <TaskList tasks={this.state.tasks} onTaskChanged={this.onTaskChanged} />
-      </div>
-    );
-  }
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>React Tasks</p>
+      </header>
+      <TaskList tasks={tasks} onTaskChanged={onTaskChanged} />
+    </div>
+  );
 }
 
 render(<Demo />, document.querySelector('#demo'));
